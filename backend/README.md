1816e2f7d00be8ceab2dab25e500848677bfd1ba
Peerawut Smithiprechawong <peerawut.s@codium.co>
2018-03-08 12:58:18 +0700

    initial project for backend part

---
+++ backend/README.md
@@ -0,0 +1,8 @@
+# HERO-FILES Backend
+
+### Requirements
+- Python 3.6.4
+- Django 2.0.1
+- Postgres 10
+
+###
---
+++ backend/herofiles/config/routers/api_urls.py
@@ -0,0 +1,6 @@
+from django.urls import path
+#from herofiles.apps.testapp import views
+
+urlpatterns = [
+    #path('', views.index, name='index'),
+]
